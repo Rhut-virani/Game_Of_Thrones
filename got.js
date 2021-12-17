d2ff3496fe276210487980e2bd28a97797f41fab
Rhut Virani <rhut.virani@gmail.com>
2021-12-17 16:23:43 -0500

    Fixed: Incorrect bg-image
    - fixed incorrect background image

--- got.js
+++ got.js
@@ -1,199 +1,283 @@
-var quotes = [
-                [
-                    ['./static/photos/logo/stark-logo.png','#565656'],
-                    ["'When the snows fall and the white winds blow, the lone wolf dies but the pack survives.'" , "- Ned Stark"],
-                    ["'There is only one war that matters. The Great War. And it is here.'" , "- Jon Snow"],
-                    ["'Nothing's worth anything to dead men.'" , "- Arya Stark"],
-                    ["'You'Re Going To Die Tomorrow, Lord Bolton. Sleep Well.'", "- Sansa Stark"],
-                    ["'The only time a man can be brave is when he is afraid.'", "- Ned Stark"],
-                    ["'Who taught you to do that?', 'No one'","- Arya Stark"],
-                    ["'Everything before the word ‘but’ is horse-shit.'", "- Ned Stark"],
-                    ["'Thank you for all your many lessons, lord Baelish. I will never forget them.'"," - Sansa Stark"],
-                    ["'What Do We Say to the God of Death?' 'Not Today'", "- Arya Stark"],
-                    ["'The man who passes the sentence should swing the sword.'", "— Ned Stark"],
-                    ["'I grew up with soldiers. I learned how to die a long time ago.'", "— Ned Stark"],
-                    ["'I'M A Slow Learner, It'S True. But I Learn.'" ,"- Sansa Stark"],
-                    ["'When people ask you what happened here, tell them the North remembers. Tell them winter came for House Frey.'","- Arya Stark"],
-                    ["'Nothing isn’t better or worse than anything. Nothing is just nothing.'","- Arya Stark"],
-                    
-                ],
-                [
-                    ['./static/photos/logo/martell-logo.png','#e12812'],
-                    ["'Lannisters aren't the only ones who pay their debts.'", "-Oberyn Martell"],
-                    ["'It is rare to meet a Lannister who shares my enthusiasm for dead Lannisters.'", "-Oberyn Martell"],
-                ],
-                [   
-                    ['./static/photos/logo/tully-logo.png','#00173d'],
-                    ["'As long as I'm standing, the war is not over.'", "-Blackfish"],
-                    ["'Bargaining with oathbreakers is like building on quicksand.'", "-Blackfish "],
-                    ["'I haven't had a proper swordfight in years. I expect I'll make a damn fool of myself.'", "-Blackfish"],
-                
-                ],
-                [
-                    ['./static/photos/logo/baratheon-logo.png','#ffe147'],
-                    ["'Killing things clears my head.'","-Robert Baratheon"],
-                    ["'I never loved my brothers. Sad thing for a man to admit, but it's true. You were the brother I chose.'","-Robert Baratheon"],
-                    ["'I hate a good many things, but I suffer them all the same.'","-Stannis Baratheon"],
-                    ["'Go on, do your duty.'","-Stannis Baratheon"],
-                    ["'We march to victory or we march to defeat, but we go forward.'","- Stannis Baratheon"],
-                    ["'I don't care, you're my friend.'","-Shireen Baratheon"],
-                    ["'I will not become a page in someone else’s history book.'","-Stannis Baratheon"],
-                    
-                ],
-                [
-                    ['./static/photos/logo/targaryen-logo.png','#000'],
-                    ["'A mother does not flee without her children.'", "-Daenerys Targaryen"],
-                    ["'Yes. All men must die, but we are not men.'", "-Daenerys Targaryen"],
-                    ["'I will answer injustice with justice.'", "-Daenerys Targaryen"],
-                    ["'I'm not going to stop the wheel... I'm going to break the wheel!'", "-Daenerys Targaryen"],
-                ],
-                [
-                    ['./static/photos/logo/arryn-logo.png','#121745'],
-                    ["'Mummy, I want to see the bad man fly.'","-Robin Arryn"],
-                    ["'I wanted to make the little Lannister baby man fly, but Mother said I couldn't.'","-Robin Arryn"],
-                    ["'She's my cousin... we should help her.'","-Robin Arryn"],
+let quotes = [
+		[
+			["./static/photos/logo/stark-logo.png", "#565656"],
+			[
+				"'When the snows fall and the white winds blow, the lone wolf dies but the pack survives.'",
+				"- Ned Stark",
+			],
+			[
+				"'There is only one war that matters. The Great War. And it is here.'",
+				"- Jon Snow",
+			],
+			["'Nothing's worth anything to dead men.'", "- Arya Stark"],
+			[
+				"'You'Re Going To Die Tomorrow, Lord Bolton. Sleep Well.'",
+				"- Sansa Stark",
+			],
+			[
+				"'The only time a man can be brave is when he is afraid.'",
+				"- Ned Stark",
+			],
+			["'Who taught you to do that?', 'No one'", "- Arya Stark"],
+			["'Everything before the word ‘but’ is horse-shit.'", "- Ned Stark"],
+			[
+				"'Thank you for all your many lessons, lord Baelish. I will never forget them.'",
+				" - Sansa Stark",
+			],
+			["'What Do We Say to the God of Death?' 'Not Today'", "- Arya Stark"],
+			[
+				"'The man who passes the sentence should swing the sword.'",
+				"— Ned Stark",
+			],
+			[
+				"'I grew up with soldiers. I learned how to die a long time ago.'",
+				"— Ned Stark",
+			],
+			["'I'M A Slow Learner, It'S True. But I Learn.'", "- Sansa Stark"],
+			[
+				"'When people ask you what happened here, tell them the North remembers. Tell them winter came for House Frey.'",
+				"- Arya Stark",
+			],
+			[
+				"'Nothing isn’t better or worse than anything. Nothing is just nothing.'",
+				"- Arya Stark",
+			],
+		],
+		[
+			["./static/photos/logo/martell-logo.png", "#e12812"],
+			[
+				"'Lannisters aren't the only ones who pay their debts.'",
+				"-Oberyn Martell",
+			],
+			[
+				"'It is rare to meet a Lannister who shares my enthusiasm for dead Lannisters.'",
+				"-Oberyn Martell",
+			],
+		],
+		[
+			["./static/photos/logo/tully-logo.png", "#00173d"],
+			["'As long as I'm standing, the war is not over.'", "-Blackfish"],
+			[
+				"'Bargaining with oathbreakers is like building on quicksand.'",
+				"-Blackfish ",
+			],
+			[
+				"'I haven't had a proper swordfight in years. I expect I'll make a damn fool of myself.'",
+				"-Blackfish",
+			],
+		],
+		[
+			["./static/photos/logo/baratheon-logo.png", "#ffe147"],
+			["'Killing things clears my head.'", "-Robert Baratheon"],
+			[
+				"'I never loved my brothers. Sad thing for a man to admit, but it's true. You were the brother I chose.'",
+				"-Robert Baratheon",
+			],
+			[
+				"'I hate a good many things, but I suffer them all the same.'",
+				"-Stannis Baratheon",
+			],
+			["'Go on, do your duty.'", "-Stannis Baratheon"],
+			[
+				"'We march to victory or we march to defeat, but we go forward.'",
+				"- Stannis Baratheon",
+			],
+			["'I don't care, you're my friend.'", "-Shireen Baratheon"],
+			[
+				"'I will not become a page in someone else’s history book.'",
+				"-Stannis Baratheon",
+			],
+		],
+		[
+			["./static/photos/logo/targaryen-logo.png", "#000"],
+			["'A mother does not flee without her children.'", "-Daenerys Targaryen"],
+			["'Yes. All men must die, but we are not men.'", "-Daenerys Targaryen"],
+			["'I will answer injustice with justice.'", "-Daenerys Targaryen"],
+			[
+				"'I'm not going to stop the wheel... I'm going to break the wheel!'",
+				"-Daenerys Targaryen",
+			],
+		],
+		[
+			["./static/photos/logo/arryn-logo.png", "#121745"],
+			["'Mummy, I want to see the bad man fly.'", "-Robin Arryn"],
+			[
+				"'I wanted to make the little Lannister baby man fly, but Mother said I couldn't.'",
+				"-Robin Arryn",
+			],
+			["'She's my cousin... we should help her.'", "-Robin Arryn"],
+		],
+		[
+			["./static/photos/logo/greyjoy-logo.png", "#000"],
+			["'I'm not fit to rule.'", "-Theon Greyjoy"],
+			[
+				"'Are you afraid?,' 'Good. It means you're not stupid.'",
+				"-Theon Greyjoy",
+			],
+			[
+				"'I would have taken you all the way to the Wall. I would have died to get you there.'",
+				"-Theon Greyjoy",
+			],
+			[
+				"'I'm going to find my little brother and I'm going to bring him home.'",
+				"-Yara Greyjoy",
+			],
+			["'Not for me... For Yara!'", "-Theon Greyjoy"],
+			[
+				"'No man has ever died from bending his knee. He who kneels may rise again, blade in hand. He who will not kneel stays dead, stiff legs and all.'",
+				"-Balon Greyjoy",
+			],
+		],
+		[
+			["'./static/photos/logo/tyrell-logo.png'", "#050"],
+			["'The trial can wait. We all need to leave.'", "- Margaery Tyrell"],
+			[
+				"'You don't think I'd let you marry that beast, do you?'",
+				"- Olenna Tyrell",
+			],
+			[
+				"'Sometimes severity is the price we pay for greatness.'",
+				"- Margaery Tyrell",
+			],
+			["'Tell Cersei. I want her to know it was me.'", "- Olenna Tyrell"],
+			[
+				"'The world is overflowing with horrible things, but they’re all a tray of cakes next to death.'",
+				"- Olenna Tyrell",
+			],
+		],
+		[
+			["'./static/photos/logo/lannister-logo.png'", "#e12812"],
+			[
+				"'Never forget what you are. The rest of the world will not. Wear it like armor, and it can never be used to hurt you.'",
+				"- Tyrion Lannister",
+			],
+			[
+				"'Any man who must say, ‘I am the king,’ is no true king. I’ll make sure you understand that when I’ve won your war for you.'",
+				"- Tywin Lannister",
+			],
+			["'The things I do for love.'", "—Jaime Lannister"],
+			["'That’s what I do: I drink and I know things.'", "—Tyrion Lannister"],
+			[
+				"'When you play the game of thrones, you win or you die. There is no middle ground.'",
+				"Cersei Lannister",
+			],
+			[
+				"'Well, my brother has his sword, and I have my mind, and a mind needs books like a sword needs a whetstone.'",
+				"Tyrion Lannister",
+			],
+			[
+				"'Everyone who makes a joke about a dwarf’s height thinks he’s the only person ever to make a joke about a dwarf’s height.'",
+				"— Tyrion Lannister",
+			],
+			[
+				"'A lion doesn’t concern himself with the opinions of the sheep.'",
+				"- Tywin Lannister",
+			],
+		],
+	],
+	firstTime = true,
+	animate = 20,
+	animateneg = "-=10",
+	qanimation = "-=12",
+	qset = "-=10.5",
+	repeatCount = Math.floor(Math.random() * (9 - 0) + 0),
+	arrayCount = 0,
+	randomIndex = 0;
+(qText = ""), (qNameText = ""), (logoUrl = ""), (logoColor = "");
 
-                ],
-                [
-                    ['./static/photos/logo/greyjoy-logo.png','#000'],
-                    ["'I'm not fit to rule.'","-Theon Greyjoy"],
-                    ["'Are you afraid?,' 'Good. It means you're not stupid.'","-Theon Greyjoy"],
-                    ["'I would have taken you all the way to the Wall. I would have died to get you there.'","-Theon Greyjoy"],
-                    ["'I'm going to find my little brother and I'm going to bring him home.'","-Yara Greyjoy"],
-                    ["'Not for me... For Yara!'","-Theon Greyjoy"],
-                    ["'No man has ever died from bending his knee. He who kneels may rise again, blade in hand. He who will not kneel stays dead, stiff legs and all.'","-Balon Greyjoy"],
-                ],
-                [
-                    ["'./static/photos/logo/tyrell-logo.png'",'#050'],
-                    ["'The trial can wait. We all need to leave.'" , "- Margaery Tyrell"],
-                    ["'You don't think I'd let you marry that beast, do you?'" , "- Olenna Tyrell"],
-                    ["'Sometimes severity is the price we pay for greatness.'" , "- Margaery Tyrell"],
-                    ["'Tell Cersei. I want her to know it was me.'" , "- Olenna Tyrell"],
-                    ["'The world is overflowing with horrible things, but they’re all a tray of cakes next to death.'" , "- Olenna Tyrell"],
-                ],
-                [
-                    ["'./static/photos/logo/lannister-logo.png'",'#e12812'],
-                    ["'Never forget what you are. The rest of the world will not. Wear it like armor, and it can never be used to hurt you.'", "- Tyrion Lannister"],
-                    ["'Any man who must say, ‘I am the king,’ is no true king. I’ll make sure you understand that when I’ve won your war for you.'", "- Tywin Lannister"],
-                    ["'The things I do for love.'", "—Jaime Lannister" ],
-                    ["'That’s what I do: I drink and I know things.'", "—Tyrion Lannister"],
-                    ["'When you play the game of thrones, you win or you die. There is no middle ground.'", "Cersei Lannister"],
-                    ["'Well, my brother has his sword, and I have my mind, and a mind needs books like a sword needs a whetstone.'", "Tyrion Lannister"],
-                    ["'Everyone who makes a joke about a dwarf’s height thinks he’s the only person ever to make a joke about a dwarf’s height.'", "— Tyrion Lannister"],
-                    ["'A lion doesn’t concern himself with the opinions of the sheep.'", "- Tywin Lannister"],    
-                ],
-            ],
-    firstTime = true,
-    animate = 20,
-    animateneg = "-=10",
-    qanimation = "-=12",
-    qset = "-=10.5",
-    repeatCount = Math.floor(Math.random() * (9 - 0) + 0),
-    arrayCount = 0,
-    randomIndex = 0 ;
-    qText = '',
-    qNameText = '',
-    logoUrl = '',
-    logoColor = ''
+$(document).ready(() => {
+	// Countdown timer
 
-    
+	let eventTime = moment
+			.utc("20-05-2019 02:25:00", "DD-MM-YYYY HH:mm:ss")
+			.unix(),
+		currentTime = moment().unix(),
+		diffTime = currentTime - eventTime,
+		duration = moment.duration(diffTime * 1000, "milliseconds"),
+		interval = 1000;
+	// if time to countdown
 
+	let $d = $(".days .text"),
+		$h = $(".hours .text"),
+		$m = $(".minutes .text"),
+		$s = $(".seconds .text"),
+		s,
+		zero = false;
+	if (diffTime > 0) {
+		// Show clock
 
-$(document).ready( ()=>{
+		setInterval(function () {
+			duration = moment.duration(
+				duration.asMilliseconds() + interval,
+				"milliseconds"
+			);
+			let d = Math.floor(moment.duration(duration).asDays()),
+				h = moment.duration(duration).hours(),
+				m = moment.duration(duration).minutes();
+			s = moment.duration(duration).seconds();
+			d = $.trim(d).length === 1 ? "0" + d : d;
+			h = $.trim(h).length === 1 ? "0" + h : h;
+			m = $.trim(m).length === 1 ? "0" + m : m;
+			s = $.trim(s).length === 1 ? "0" + s : s;
+			// show how many hours, minutes and seconds are left
+			$d.text(d);
+			$h.text(h);
+			$m.text(m);
+			$s.text(s);
+		}, interval);
+	}
+	animatenumbers = () => {
+		let an = new TimelineMax()
+			.to(".small", 0.25, { autoAlpha: 1 })
+			.to(".text", 1, { autoAlpha: 1 });
+	};
 
-    // Countdown timer
+	onRepeat = () => {
+		repeatCount++;
+		arrayCount++;
+		if (repeatCount < 9) {
+			randomIndex = Math.floor(
+				Math.random() * (quotes[repeatCount].length - 1) + 1
+			);
+			qText = quotes[repeatCount][randomIndex][0];
+			qNameText = quotes[repeatCount][randomIndex][1];
+			logoUrl = quotes[repeatCount][0][0];
+			logoColor = quotes[repeatCount][0][1];
+			animation(qText, qNameText, logoUrl, logoColor);
+		} else {
+			repeatCount = 0;
+			randomIndex = Math.floor(
+				Math.random() * (quotes[repeatCount].length - 1) + 1
+			);
+			qText = quotes[repeatCount][randomIndex][0];
+			qNameText = quotes[repeatCount][randomIndex][1];
+			logoUrl = quotes[repeatCount][0][0];
+			logoColor = quotes[repeatCount][0][1];
+			animation(qText, qNameText, logoUrl, logoColor);
+		}
+	};
 
-    var eventTime = moment.utc('20-05-2019 02:25:00', 'DD-MM-YYYY HH:mm:ss').unix(),
-        currentTime = moment().unix(),
-        diffTime = currentTime - eventTime,
-        duration = moment.duration(diffTime * 1000, 'milliseconds'),
-        interval = 1000;
-    // if time to countdown
-    if(diffTime > 0) {
+	// Changing color and quotes animation.....
 
-        // Show clock
-
-
-        var $d = $('.days .text'),
-            $h = $('.hours .text'),
-            $m = $('.minutes .text'),
-            $s = $('.seconds .text'),
-            s,
-            zero = false;
-
-
-        setInterval(function(){
-
-            duration = moment.duration(duration.asMilliseconds() + interval, 'milliseconds');
-            var d = Math.floor(moment.duration(duration).asDays()),
-                h = moment.duration(duration).hours(),
-                m = moment.duration(duration).minutes();
-                s = moment.duration(duration).seconds();
-            d = $.trim(d).length === 1 ? '0' + d : d;
-            h = $.trim(h).length === 1 ? '0' + h : h;
-            m = $.trim(m).length === 1 ? '0' + m : m;
-            s = $.trim(s).length === 1 ? '0' + s : s;
-            // show how many hours, minutes and seconds are left
-            $d.text(d);
-            $h.text(h);
-            $m.text(m);
-            $s.text(s);
-
-        }, interval);
-
-    }
-    animatenumbers = () => {
-        var an = new TimelineMax()
-            .to('.small', 0.25, {autoAlpha:1})
-            .to('.text', 1, {autoAlpha:1})
-    }
-
-    onRepeat = () => {
-        repeatCount++;
-        arrayCount++;
-        if(repeatCount < 9){
-            randomIndex = Math.floor(Math.random() * (quotes[repeatCount].length - 1) + 1);
-            qText = quotes[repeatCount][randomIndex][0];
-            qNameText = quotes[repeatCount][randomIndex][1];
-            logoUrl = quotes[repeatCount][0][0];
-            logoColor = quotes[repeatCount][0][1];
-            animation(qText, qNameText, logoUrl, logoColor);
-        }
-        else{
-            repeatCount = 0;
-            randomIndex = Math.floor(Math.random() * (quotes[repeatCount].length - 1) + 1);
-            qText = quotes[repeatCount][randomIndex][0];
-            qNameText = quotes[repeatCount][randomIndex][1];
-            logoUrl = quotes[repeatCount][0][0];
-            logoColor = quotes[repeatCount][0][1];
-            animation(qText, qNameText, logoUrl, logoColor);
-        }
-    }
-
-    // Changing color and quotes animation.....
-
-    animation = ()=>{
-
-        var bgcolor = new TimelineMax({paused:true, onComplete: onRepeat})
-            .to('.color', animate, {backgroundColor:logoColor})
-            .to('.quotes', 1,{autoAlpha: 0}, qanimation)
-            .to('.logo', 1, {autoAlpha:0}, qanimation)
-            .set('.q',{text:qText}, qset)
-            .set('.qName',{text:qNameText}, qset)
-            .set('.logo',{backgroundImage:`url(${logoUrl})`}, qset)
-            .to('.quotes', 1,{autoAlpha: 1},animateneg)
-            .to('.logo', 1, {autoAlpha:0.2}, animateneg)
-        bgcolor.play();
-
-    }
-    // onRepeat();
-    setInterval(() => {
-        animatenumbers();
-        (s % 20 === 0) ? zero=true: false;
-        if(firstTime && zero){
-            onRepeat();
-            firstTime = false;
-        }
-    }, interval);
-
-})
+	animation = () => {
+		let bgcolor = new TimelineMax({ paused: true, onComplete: onRepeat })
+			.to(".color", animate, { backgroundColor: logoColor })
+			.to(".quotes", 1, { autoAlpha: 0 }, qanimation)
+			.to(".logo", 1, { autoAlpha: 0 }, qanimation)
+			.set(".q", { text: qText }, qset)
+			.set(".qName", { text: qNameText }, qset)
+			.set(".logo", { backgroundImage: `url(${logoUrl})` }, qset)
+			.to(".quotes", 1, { autoAlpha: 1 }, animateneg)
+			.to(".logo", 1, { autoAlpha: 0.2 }, animateneg);
+		bgcolor.play();
+	};
+	// onRepeat();
+	setInterval(() => {
+		animatenumbers();
+		s % 20 === 0 ? (zero = true) : false;
+		if (firstTime && zero) {
+			onRepeat();
+			firstTime = false;
+		}
+	}, interval);
+});
